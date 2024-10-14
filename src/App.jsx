c96eaa66ea7f8795d9eb2e2ade9e2a2ef77893c4
Mohamed elashry <109392180+mel34ryy@users.noreply.github.com>
2024-10-14 16:45:32 +0300

    Resolved merge conflicts

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Products from "./components/Products/Products";
+import Contact from "./components/Contact/Contact";
+import Profile from "./components/Profile/Profile";
+import NotFound from "./components/NotFound/NotFound";
+
+function App() {
+  let routers = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "home", element: <Home /> },
+        { path: "products", element: <Products /> },
+        { path: "contact", element: <Contact /> },
+        { path: "profile", element: <Profile /> },
+        { path: "*", element: <NotFound /> },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={routers}></RouterProvider>
+    </>
+  );
+}
+
+export default App;
